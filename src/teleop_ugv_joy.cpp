95eb1cb04e2ae3da0f1666dd1d9c90c7c5e3b170
bsb808 <briansbingham@gmail.com>
2016-05-19 05:48:36 -0700

    working version with P3!

--- src/teleop_ugv_joy.cpp
+++ src/teleop_ugv_joy.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include "geometry_msgs/Twist.h"
 #include <sensor_msgs/Joy.h>
+#include <stdio.h>
 
 
 class TeleopUgv
@@ -23,17 +24,17 @@ private:
 
 TeleopUgv::TeleopUgv():
   linear_(1),
-  angular_(2)
+  angular_(0),
+  l_scale_(1),
+  a_scale_(1)
 {
-
-  nh_.param("axis_linear", linear_, linear_);
-  nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
-
-
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("ugv/cmd_vel", 1);
-
+  // Get the private (~) parameters
+  ros::param::param("~axis_linear", linear_, linear_);
+  ros::param::param("~axis_angular", angular_, angular_);
+  ros::param::param("~scale_angular", a_scale_, a_scale_);
+  ros::param::param("~scale_linear", l_scale_, l_scale_);
+  printf("Parameters: axis_linear=%d, axis_angular=%d, scale_angular=%f, scale_linear=%f\n",linear_,angular_,a_scale_,l_scale_);
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("joy/cmd_vel", 1);
 
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopUgv::joyCallback, this);
 
